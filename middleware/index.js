582de502c92e4ad9020f565cc410e08a54bb6e98
mehdithe <mehdi.cheracher@gmail.com>
2018-10-12 09:22:54 +0100

    added the middleware exercice

---
+++ middleware/index.js
@@ -0,0 +1,42 @@
+const url = require("url")
+
+class Response {}
+
+class Request {}
+
+class Express {
+
+  constructor() {}
+
+  // this function should register the middleware given as parameter
+  // see the example given below
+  use(fn) {}
+  
+  // complete the code for get so that all registered
+  // middleware are called appropriateley, and if an error is
+  // invoked, you should stop the chain and just print error
+  get(url, callback) {}
+}
+
+let app = new Express()
+
+
+app.use((req, res, next) => {
+  console.log('in the first middleware')
+  req.body = {
+    name: 'mehdi', 
+    age: 24
+  }
+  next() // calling next is very important
+})
+
+app.use((req, res, next) => {
+  console.log('in the second middleware')
+  req.query = url.parse('/home?key=value', true)
+  next() // calling next is very important
+})
+
+app.get('/route', (req, res) => {
+  console.log("inside the route")
+  console.log(req, res)
+})
